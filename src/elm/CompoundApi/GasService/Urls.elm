078b57a888d7f581282ea1ed572a1222ec0730bb
Kevin J <qaz111id@gmail.com>
2023-03-06 15:09:25 -0600

    migrate get gas price endpoint to v3 api (#62)

--- src/elm/CompoundApi/GasService/Urls.elm
+++ src/elm/CompoundApi/GasService/Urls.elm
@@ -1,10 +1,11 @@
 module CompoundApi.GasService.Urls exposing (getGasPriceUrl)
-
-import CompoundApi.Common.Url
-import CompoundComponents.Eth.Network exposing (Network)
+import CompoundComponents.Eth.Network exposing (Network, networkName)
+import Url.Builder as UrlBuilder
+import String exposing (toLower)
 import Dict exposing (Dict)
 
 
 getGasPriceUrl : Dict String String -> Network -> Maybe String
 getGasPriceUrl apiBaseUrlMap network =
-    CompoundApi.Common.Url.buildApiUrl apiBaseUrlMap network "gas_prices/get_gas_price" []
+    Dict.get "v3_api" apiBaseUrlMap
+        |> Maybe.map (\apiBaseUrl -> apiBaseUrl ++ UrlBuilder.relative [ "legacy", toLower (networkName network), "gas-price" ] [])
