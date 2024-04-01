bc5bcaab15bca71de6b304c9a2993f660449ec1a
professortX <154901473+professortX@users.noreply.github.com>
2024-04-02 00:22:20 +0800

    fix: frontend related (non-functional) bugs - part 2 (#16)
    * feat: ip dmclivenet
    
    * update version
    
    * fix: fix header and redirection link
    
    * fix: fix some ui issues
    
    * fix: fix ui bugs
    
    * fix: fix ui issues

--- src/elm/DappInterface/PrimaryActionModal.elm
+++ src/elm/DappInterface/PrimaryActionModal.elm
@@ -572,9 +572,9 @@ assetAndCompRateForm userLanguage config maybeEtherUsdPrice ({ chosenAsset, prim
                     "– %"
     in
     div [ class "form" ]
-        [ a ([ class "label-link", target "__blank" ] ++ href External "https://app.compound.finance/markets/?market=v2")
+        [ div ([ class ""])
             [ label [ class "dark" ] [ text formLabel ]
-            , div [ class "line-icon line-icon--small line-icon--external-link line-icon--external-link--black" ] []
+
             ]
         , div [ class "calculation" ]
             [ span []
